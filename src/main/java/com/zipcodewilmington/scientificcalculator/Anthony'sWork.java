835d2f1644a2ef7ff6478964a2928d3a91f561be
AnthonyF525 <Anthony.falice@yahoo.com>
2025-07-13 11:13:31 -0400

    Adding comments

--- src/main/java/com/zipcodewilmington/scientificcalculator/Anthony'sWork.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Anthony'sWork.java
@@ -16,18 +16,18 @@ class Switch {
         
 
         switch (mode.toLowerCase()) {
-            case "decimal":
+            case "decimal": // Select Decimal mode
                 System.out.println(mode + " mode selected.");
                 break;
-            case "binary":
+            case "binary": // Select Binary mode
                 System.out.println(mode + " mode selected.");
                 System.out.println(Integer.toBinaryString(sd.nextInt()));
                 break;
-            case "octal":
+            case "octal": // Select Octal mode
                 System.out.println(mode + " mode selected.");
                 System.out.println(Integer.toOctalString(sd.nextInt()));
                 break;
-            case "hex":
+            case "hex": // Select Hexadecimal mode
                 System.out.println(mode + " mode selected.");
                 System.out.println(Integer.toHexString(sd.nextInt()));
                 break;
@@ -59,34 +59,34 @@ class Trig {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Radian or Degree? ");
+        System.out.println("Radian or Degree? "); // Ask for Radian or Degree
         String radianOrDegree = sc.nextLine();
 
         if (radianOrDegree.equalsIgnoreCase("degree")){
-            System.out.println("Which Operation? (sin, cos, tan, asin, acos, atan):");
+            System.out.println("Which Operation? (sin, cos, tan, asin, acos, atan):"); //Ask for the operation
             String operation = sc.nextLine();
             switch (operation) {
-                case "sin":
+                case "sin": // Perform sine operation, Radian
                     System.out.print("What number do you want in the sin? ");
                     System.out.println(Math.sin(Math.toRadians(sc.nextInt())));
                     break;
-                case "cos":
+                case "cos": // Perform cosine operation, Radian
                     System.out.print("What number do you want in the cos? ");
                     System.out.println(Math.cos(Math.toRadians(sc.nextInt())));
                     break;
-                case "tan":
+                case "tan": // Perform tangent operation, Radian
                     System.out.print("What number do you want in the tan? ");
                     System.out.println(Math.tan(Math.toRadians(sc.nextInt())));
                     break;
-                case "asin":
+                case "asin": // Perform arcsine operation, Radian
                     System.out.print("What number do you want in the asin? ");
                     System.out.println(Math.asin(Math.toRadians(sc.nextInt())));
                     break;
-                case "acos":
+                case "acos": // Perform arccosine operation, Radian
                     System.out.print("What number do you want in the acos? ");
                     System.out.println(Math.acos(Math.toRadians(sc.nextInt())));
                     break;
-                case "atan":
+                case "atan": // Perform arctangent operation, Radian 
                     System.out.print("What number do you want in the atan? ");
                     System.out.println(Math.atan(Math.toRadians(sc.nextInt())));
                     break;
@@ -97,29 +97,29 @@ class Trig {
             System.out.println("Which Operation? (sin, cos, tan, asin, acos, atan):");
             String operation = sc.nextLine();
             switch (operation) {
-                case "sin":
+                case "sin": // Perform sine operation
                     System.out.print("What number do you want in the sin? ");
                     System.out.println(Math.sin(sc.nextInt()));
                     break;
-                case "cos":
+                case "cos": // Perform cosine operation
                     System.out.print("What number do you want in the cos? ");
                     System.out.println(Math.cos(sc.nextInt()));
                     break;
-                case "tan":
+                case "tan": // Perform tangent operation
                     System.out.print("What number do you want in the tan? ");
                     System.out.println(Math.tan(sc.nextInt()));
                     break;
-                case "asin":
+                case "asin": // Perform arcsine operation
                     System.out.print("What number do you want in the asin? ");
-                    System.out.println(Math.asin(Math.toRadians(sc.nextInt())));
+                    System.out.println(Math.asin(sc.nextInt()));
                     break;
-                case "acos":
+                case "acos": // Perform arccosine operation
                     System.out.print("What number do you want in the acos? ");
-                    System.out.println(Math.acos(Math.toRadians(sc.nextInt())));
+                    System.out.println(Math.acos(sc.nextInt()));
                     break;
-                case "atan":
+                case "atan": // Perform arctangent operation
                     System.out.print("What number do you want in the atan? ");
-                    System.out.println(Math.atan(Math.toRadians(sc.nextInt())));
+                    System.out.println(Math.atan(sc.nextInt()));
                     break;
                 default:
                     break;
