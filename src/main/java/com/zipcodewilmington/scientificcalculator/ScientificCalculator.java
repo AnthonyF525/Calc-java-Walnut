9576d81e803215253550836badca8876f1723a3e
Abubakaryounis97 <Abubakaryounis97@gmail.com>
2025-07-13 01:11:01 -0400

    younis changes 1

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificCalculator.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.zipcodewilmington.scientificcalculator;
+
+
+class ScientificCalculator 
+{
+
+    public static long factorial(long x) //method to calculate factorial
+    {
+        if (x<0)
+        {
+        throw new IllegalArgumentException("Factroial not defined for negative numbers");
+        }
+        long result =1;  // factorial of zero is always zero 
+        for (int i=1;i <= x;i++)
+        {   
+        result = result *i;
+        }
+        return result;
+
+    }
+
+    public static double logbase10(double x) // mehtod to calculate log base 10
+    {
+        if (x <= 0)
+        {
+        throw new IllegalArgumentException("Err");
+        }
+        return Math.log10(x);  // return log10(X)
+    }
+
+    public static double Inverselog10(double x) // method to calculate inverse log 10
+    {
+           
+        return Math.log(x);  // return inverse log base 10
+    }
+
+    public static double ln(double x)  // method to calculate natural log
+    {
+        if (x <= 0)
+        {
+        throw new IllegalArgumentException("Err");
+        }
+        return Math.exp(x);  // return inverse ln(x)
+
+    }
+    public static double Inverseln(double x) // method to calculate inverse log
+    {
+        
+        return Math.log(x);  // return ln(x)
+
+    }
+
+    private double memory = 0.0; // starting value of memeory
+    
+        public void memoryStorage(double x)
+        {
+        memory = x;
+        }
+        public void memoryClear()
+        {
+        memory= 0.0;
+        }
+        public double memoryRecall()
+        {
+        return memory;
+        }
+
+
+    }
+
+    
+
+
+}
+
+
